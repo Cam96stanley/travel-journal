cbe73b296b8642daa9e998187da71f64c9301446
cam96stanley <cam96stanley@gmail.com>
2024-10-24 22:11:52 -0400

    added asset images and data.js file with data for locations

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    title: "Empire State Building",
+    location: "USA",
+    googleMapsUrl: "https://maps.app.goo.gl/jM1iNzJghmvV2YWZA",
+    startDate: "10 Nov, 2022",
+    endDate: "12 Nov, 2022",
+    description:
+      "The Empire State Building, completed in 1931 in New York City, stands 1,454 feet tall, including its antenna. Designed by William F. Lamb in the Art Deco style, it was the tallest building in the world until 1977 and remains a major cultural icon, attracting millions of visitors annually.",
+    imageUrl: "./src/assets/empire-state-bldg.jpg",
+  },
+  {
+    title: "St. Basil's Cathedral",
+    location: "Russia",
+    googleMapsUrl: "https://maps.app.goo.gl/jbAf1KzYcSfg8pzSA",
+    startDate: "06 Jul, 2003",
+    endDate: "10 Jul, 2003",
+    description:
+      "St. Basil's Cathedral, built from 1555 to 1561 in Moscow's Red Square, features nine onion-shaped domes. Designed by architects Postnik Yakovlev and Ivan Shiryayev, it symbolizes the Russian Orthodox Church.",
+    imageUrl: "./src/assets/cathedral.jpg",
+  },
+  {
+    title: "USS Constitution",
+    location: "USA",
+    googleMapsUrl: "https://maps.app.goo.gl/37abQzcQuQEjnFF4A",
+    startDate: "23 Oct, 2022",
+    endDate: "25 Oct, 2022",
+    description:
+      "The USS Constitution, launched in 1797, is the oldest commissioned warship afloat in the world. It served in the War of 1812 and is famous for its impressive victories against British ships. Located in Boston, it remains a symbol of American naval history and is part of the National Park Service.",
+    imageUrl: "./src/assets/uss-constitution.jpg",
+  },
+];
